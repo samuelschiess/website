55c2cd8b0eb0086bb4e75e14146e2cfe950a979a
samuelschiess <92222690+samuelschiess@users.noreply.github.com>
2022-05-11 18:08:14 -0600

    Add files via upload

---
+++ tubaSite/tubaSite/posture.js
@@ -0,0 +1,12 @@
+console.log("Hello World");
+console.log("Tension = bad sounds");
+let name = 'Patrick Sheridan';
+console.log(name);
+
+let person = {
+    name: 'Mosh',
+    age: 30
+};
+
+person.name = 'Josh';
+console.log(person.name);
